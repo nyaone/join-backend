f31f36c8eb6584d3cbd64eb105de6e7ab6bb0e4d
Nya Candy <dev@candinya.com>
2023-01-14 15:11:07 +0800

    feat: longer sessioin valid time

--- consts/time.go
+++ consts/time.go
@@ -3,8 +3,8 @@ package consts
 import "time"
 
 const (
-	TIME_LOGIN_REQUEST_VALID = 3 * time.Minute
-	TIME_LOGIN_SESSION_VALID = 6 * time.Hour
+	TIME_LOGIN_REQUEST_VALID = 10 * time.Minute
+	TIME_LOGIN_SESSION_VALID = 24 * time.Hour
 
 	TIME_NEW_USER_SEND_INVITATION_AFTER = 24 * time.Hour
 )
